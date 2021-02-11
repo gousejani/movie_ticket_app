dc5fb4c612c4e12d904bfb6b0bd03460946f52db
Gouse Jani <gousejani13@gmail.com>
2021-02-11 16:03:17 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 // Connect database
@@ -8,14 +9,19 @@ connectDB();
 // Middle ware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.send('API Started');
-});
-
 // Routes
 app.use('/api/users/', require('./routes/users'));
 app.use('/api/bookings/', require('./routes/bookings'));
 app.use('/api/movies/', require('./routes/movies'));
+
+// Serve static assests in production
+if (process.env.NODE_ENV === 'production') {
+	// Static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
